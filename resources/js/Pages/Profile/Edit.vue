b9bd3b63146f7f46ee23484e0dbda8ee69792acf
belorussky <belorussky@gmail.com>
2026-03-20 16:42:33 +0100

    Edit profile info. Update password. Delete account

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -1,5 +1,23 @@
-<script setup></script>
+<script setup lang="ts">
+import UpdateInfo from "./Sections/UpdateInfo.vue";
+import UpdatePassword from "./Sections/UpdatePassword.vue";
+import DeleteAccount from "./Sections/DeleteAccount.vue";
+
+interface User {
+    name: string;
+    email: string;
+    email_verified_at: string | null;
+}
+
+defineProps<{
+    user: User;
+    status?: string;
+}>();
+</script>
 
 <template>
-    <div>Profile</div>
+    <Head title="- Profile" />
+    <UpdateInfo :user="user" :status="status" />
+    <UpdatePassword />
+    <DeleteAccount />
 </template>
--- resources/js/fontawesome.ts
+++ resources/js/fontawesome.ts
@@ -2,8 +2,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 // solid
-import { faCircleHalfStroke, faIdBadge, faAt, faKey, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { faCircleHalfStroke, faIdBadge, faAt, faKey, faAngleDown, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCircleHalfStroke, faIdBadge, faAt, faKey, faAngleDown)
+library.add(faCircleHalfStroke, faIdBadge, faAt, faKey, faAngleDown, faTriangleExclamation)
 
 export { FontAwesomeIcon }
